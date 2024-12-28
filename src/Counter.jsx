e2aed02e1bd703c4349d4c66267b1215d078a302
Dmitry Kazantsev <dmitry_kazantsev@yahoo.com>
2024-12-28 13:32:12 +0300

    176 Fething weather data

---
+++ src/Counter.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    //console.log(this);
+
+    this.setState(curState => {
+      return { count: curState.count - 1 };
+    });
+
+    // this.setState({ count: 10 });
+  }
+
+  handleIncrement() {
+    //console.log(this);
+
+    this.setState(curState => {
+      return { count: curState.count + 1 };
+    });
+
+    // this.setState({ count: 10 });
+  }
+
+  render() {
+    const date = new Date('June 21 2027');
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>| - |</button>
+        <span>
+          {date.toDateString()} {this.state.count}
+        </span>
+        <button onClick={this.handleIncrement}>| + |</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
